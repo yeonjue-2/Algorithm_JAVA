0b0dcb395b56472c5e007675c39b25d6c7ca9c0f
박연주 <ilov-@naver.com>
2022-10-21 09:52:29 +0900

    b9020번 골드바흐의 추측(짝수는 소수 두개의 합)

--- 1000번 이상/b9020.java
+++ 1000번 이상/b9020.java
@@ -1,3 +1,57 @@
+public class Prac{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int M = Integer.parseInt(br.readLine());
+            ArrayList<Integer> primes = new ArrayList<>();
+
+            primes.add(1);
+            primes.add(1);
+
+            for (int j = 2; j < M-1; j++) {
+                primes.add(0);
+            }
+
+            int sqrt = (int) Math.sqrt(M);
+
+            for (int j = 2; j < sqrt + 1; j++) {
+                if (primes.get(j) == 0) {
+                    for (int k = j+j; k < M-1; k += j) {
+                        primes.set(k, 1);
+                    }
+                }
+            }
+
+            ArrayList<Integer> prime = new ArrayList<>();
+
+            for (int j = 2; j < primes.size(); j++) {
+                if (primes.get(j) == 0) {
+                    prime.add(j);                       // {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53}
+                }
+            }
+
+            int[] arr = new int[]{0, M};
+
+            for (int j = prime.size()-1; j >= 0; j--) {
+                int re = M - prime.get(j);
+                if (prime.contains(re) && prime.get(j) < arr[1] && prime.get(j) >= M/2) {
+                    arr[0] = re;
+                    arr[1] = prime.get(j);
+                }
+            }
+
+            System.out.println(arr[0] + " " + arr[1]);
+
+        }
+    }
+}
+
+
+
+// 다른 풀이 (시간과 메모리를 많이 줄임)
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
