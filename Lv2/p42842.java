00555136f09f4d36eb7e1e1e44e0d08129a431ec
박연주 <ilov-@naver.com>
2022-10-28 21:48:30 +0900

    42842 카펫(약수, 완전탐색)

--- Lv2/p42842.java
+++ Lv2/p42842.java
@@ -1,1 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
 
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int number = brown + yellow;
+        int sqrt = (int) Math.sqrt(number);
+
+        for (int i=3; i<sqrt+1; i++) {
+            if (number%i == 0) {
+                map.put(i, number/i);
+            }
+        }
+
+        for (int key : map.keySet()) {
+            if ((key - 2) * (map.get(key) - 2) == yellow) {
+                answer[0] = map.get(key);
+                answer[1] = key;
+            }
+        }
+
+        return answer;
+    }
+}
