fa6b38bae14589278e7d370e47bdd68b78d74458
박연주 <ilov-@naver.com>
2022-10-29 23:30:13 +0900

    120805 문자열 정렬하기(1)

---
+++ Lv.0/p120850.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+     public int[] solution(String my_string) {
+
+        my_string = my_string.replaceAll("[^0-9]", "");
+        int[] answer = new int[my_string.length()];
+
+        for (int i = 0; i <= my_string.length()-1; i++) {
+            answer[i] = Integer.parseInt(my_string.substring(i, i+1));
+        }
+
+        Arrays.sort(answer);
+         
+        return answer;
+    }
+}
