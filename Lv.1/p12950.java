f09c7a12679d1f195087e2544ef383571ac91e62
박연주 <ilov-@naver.com>
2022-11-05 00:45:41 +0900

    12950 행렬의 덧셈

---
+++ Lv.1/p12950.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Zoo {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        return answer;
+    }
+  
+  
+
+// 다른 풀이
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
+        answer = arr1;
+        for(int i=0; i<arr1.length; i++){
+            for(int j=0; j<arr1[0].length; j++){
+                answer[i][j] += arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
+
+  
+// 어차피 크기가 같으므로 arr1로 설정해 두고 arr2만 더해주면 된다.
