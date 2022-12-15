ac294f81ccced6618be4198b470801569d1b1d42
박연주 <ilov-@naver.com>
2022-12-15 13:26:30 +0900

    12981 영어 끝말잇기

--- Lv2/p12981.java
+++ Lv2/p12981.java
@@ -36,3 +36,37 @@ class Solution {
         return answer;
     }
 }
+
+
+// 리팩토링
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        List<String> word = new ArrayList<>();
+        int index = 0;
+        word.add(words[0]);
+
+        for (int i = 1; i < words.length; i++) {
+            String before = word.get(word.size()-1);
+
+            if (before.charAt(before.length()-1) == words[i].charAt(0) && !word.contains(words[i])) {
+                word.add(words[i]);
+            } else {
+                index = i + 1;
+                break;
+            }
+        }
+
+        if (index != 0) {
+            if (index % n == 0) {
+                answer[0] = n;
+            } else {
+                answer[0] = index % n;
+            }
+            answer[1] = (int) Math.ceil(index * 1.0 / n);
+        }
+
+        return answer;
+    }
+}
