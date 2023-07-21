5ff47c8fbd9d989f645017bc40fba3a5257e0666
박연주 <ilov-@naver.com>
2023-07-22 01:00:31 +0900

    코딩 테스트 답변 제출용

--- Problem.java
+++ Problem.java
@@ -2,6 +2,7 @@ class Problem {
     public int solution(int sum, int[] coin) {
         int answer = 0;
 
+        // 각 수들을 만드는 방법 수를 저장할 배열 생성
         int[] number = new int[sum+1];
         number[0] = 1;
 
@@ -20,3 +21,5 @@ class Problem {
         return answer;
     }
 }
+
+// sum = 4, coin = {1, 2, 3}
