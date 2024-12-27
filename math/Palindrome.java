9ec1adadc2a43657ebabd8d4c780409053666c12
박연주 <ilov-@naver.com>
2024-12-27 22:39:58 +0900

    자료구조 - deque

---
+++ math/Palindrome.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Palindrome {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split("");
+        Deque<String> deque = new ArrayDeque<>();
+        int isPalindrome = 1;
+
+        Arrays.stream(arr).forEach(s -> deque.addLast(s));
+
+        while (!deque.isEmpty()) {
+            String a = deque.pollFirst();
+            String b = deque.pollLast();
+
+            if (a != null & b != null && !a.equals(b)) {
+                isPalindrome = 0;
+                break;
+            }
+        }
+
+        System.out.println(isPalindrome);
+
+    }
+}
