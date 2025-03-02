aa17ebb1d6a29199f6efc42349edc8cd26efcf3f
박연주 <ilov-@naver.com>
2025-03-03 00:37:43 +0900

    Create InvertTree.java

---
+++ grind75/InvertTree.java
@@ -0,0 +1,14 @@
+class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+
+        if(root == null){
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left=invertTree(root.right);
+        root.right=invertTree(temp);
+        
+        return root;
+    }
+}
