5ddef1c6471c1a715216cfbaa08b6b0eb21641f9
박연주 <ilov-@naver.com>
2025-03-03 22:17:06 +0900

    Sorting, HashTable

--- grind75/Anagram.java
+++ grind75/Anagram.java
@@ -32,3 +32,29 @@ public class Grain75 {
     }
 }
 
+// HashTable 사용  풀이
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> count = new HashMap<>();
+        
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) - 1);
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
