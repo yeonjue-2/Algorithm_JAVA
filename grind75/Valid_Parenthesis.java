ebc1e1737f808af14321a1b7ebe1b4fb2ef63e55
박연주 <ilov-@naver.com>
2025-11-01 17:24:40 +0900

    Stack, Array
    Renamed class from Valid_Parentheses to Valid_Parenthesis and added a Python implementation for checking valid parentheses.

--- grind75/Valid_Parenthesis.java
+++ grind75/Valid_Parenthesis.java
@@ -1,6 +1,6 @@
 import java.util.Stack;
 
-class Valid_Parentheses {
+class Valid_Parenthesis {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         
@@ -43,3 +43,28 @@ class Valid_Parentheses {
 //         return stack.isEmpty();
 //     }
 // }
+
+
+/* python 구현 추가
+def is_correct_parenthesis(string):
+    stack = []
+
+    for i in range(len(string)):
+        if string[i] == '(':
+            stack.append("(")
+        elif string[i] == ')':
+            if len(stack) == 0:
+                return False
+            stack.pop()
+
+    if len(stack) != 0:
+        return False
+    else:
+        return True
+
+print("정답 = True / 현재 풀이 값 = ", is_correct_parenthesis("(())"))
+print("정답 = False / 현재 풀이 값 = ", is_correct_parenthesis(")"))
+print("정답 = False / 현재 풀이 값 = ", is_correct_parenthesis("((())))"))
+print("정답 = False / 현재 풀이 값 = ", is_correct_parenthesis("())()"))
+print("정답 = False / 현재 풀이 값 = ", is_correct_parenthesis("((())"))
+*/
